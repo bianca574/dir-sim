29cda73a642ab775c747f41e05717c9d45658066
Bianca-Ioana Ilie-Serban <bianca-ioana.ilie-serban@etu.u-paris.fr>
2026-04-16 02:27:04 +0200

    Correction boucle infinie

--- include/premiereCommande.h
+++ include/premiereCommande.h
@@ -1,5 +1,13 @@
+<<<<<<< HEAD:include/premiereCommande.h
 #include "dataNode.h"
 
 void ls();
 void mkdir(const char *nom);
 void ajouterFilsANoeudCourant(noeud *nouveauFils);
+=======
+#include "dataNode.h"
+
+void ls();
+void mkdir(const char *nom);
+void ajouterFilsANoeudCourant(noeud *nouveauFils);
+>>>>>>> 0818333 (Correction boucle infinie):premiereCommande.h
--- src/main.c
+++ src/main.c
@@ -1,3 +1,4 @@
+<<<<<<< HEAD:src/main.c
 #include <stdio.h>
 #include <stdlib.h>
 #include "dataNode.h"
@@ -31,3 +32,37 @@ int main()
 
     return 0;
 }
+=======
+#include <stdio.h>
+#include <stdlib.h>
+#include "premiereCommande.h"
+#include <stdbool.h>
+
+noeud *noeudCourant = NULL;
+
+void init()
+{
+    noeudCourant = malloc(sizeof(noeud));
+    if (noeudCourant == NULL)
+    {
+        printf("Échec d'allocation\n");
+        exit(1);
+    }
+    noeudCourant->est_dossier = true;
+    noeudCourant->nom[0] = '\0';
+    noeudCourant->pere = noeudCourant;
+    noeudCourant->racine = noeudCourant;
+    noeudCourant->fils = NULL;
+}
+
+int main()
+{
+    printf("ok\n");
+    init();
+    ls();
+    mkdir("Test");
+    ls();
+
+    return 0;
+}
+>>>>>>> 0818333 (Correction boucle infinie):main.c
