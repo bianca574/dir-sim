c34fa55d50fe662cf8207f9ee185a5bf5d9e0768
Bianca-Ioana Ilie-Serban <bianca-ioana.ilie-serban@etu.u-paris.fr>
2026-04-16 02:47:43 +0200

    Ajout de la pipeline

--- src/main.c
+++ src/main.c
@@ -1,4 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "dataNode.h"
+#include "premiereCommande.h"
+#include <stdbool.h>
+
+noeud *noeudCourant = NULL;
+
+void init()
+{
+    noeudCourant = malloc(sizeof(noeud));
+    if (noeudCourant == NULL)
+    {
+        printf("Échec d'allocation\n");
+        exit(1);
+    }
+    noeudCourant->est_dossier = true;
+    noeudCourant->nom[0] = '\0';
+    noeudCourant->pere = noeudCourant;
+    noeudCourant->racine = noeudCourant;
+    noeudCourant->fils = NULL;
+}
+
 int main()
 {
+    printf("ok\n");
+    init();
+    ls();
+    mkdir("Test");
+    ls();
+
     return 0;
 }
