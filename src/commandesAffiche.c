88500f97625e227c50a207e3521dda75fc1a6ced
Agnes <kocikagnes0@gmail.com>
2026-04-17 12:00:30 +0200

    ajout commande print

--- src/commandesAffiche.c
+++ src/commandesAffiche.c
@@ -6,16 +6,75 @@
 #include "dataNode.h"
 #include "commandesAffiche.h"
 
-void ls()
-{
+void ls(){
     liste_noeud *copie = noeudCourant->fils;
-    while (copie != NULL)
-    {
+    while (copie != NULL){
         printf("%s \n", copie->no->nom);
         copie = copie->succ;
     }
 }
 
-void pwd()
-{
+void pwd(){
 }
+
+void print_noeud(noeud *noeud){
+
+    //nom noeud
+    if (strcmp(noeud->nom,"\0")!=0){
+        printf("Noeud %s ", noeud->nom);
+    }
+    else{
+        printf("Noeud / ");
+    }
+    if (noeud->est_dossier){
+        printf("(D), ");
+    }else {
+        printf("(F), ");
+    }
+
+    //pere
+    if (noeud != noeud->racine ){
+        if (strcmp(noeud->pere->nom, "\0")==0){
+            printf("pere : /, ");
+        }
+        else {
+            printf("pere : %s ", noeud->pere->nom);
+        }
+    }
+
+    //fils
+    printf("fils : ");
+    liste_noeud *copie = noeud->fils;
+    while (copie != NULL){
+        printf("%s", copie->no->nom);
+        if (copie->no->est_dossier){
+            printf("(D)");
+        }else {
+            printf("(F)");
+        }
+       if (copie->succ != NULL ){
+            printf(", ");
+       }
+        copie = copie->succ;
+    }
+}
+
+void print_aux(noeud *noeud){
+    if (noeud ==NULL){
+        return;
+    }
+    print_noeud(noeud);
+    printf("\n");
+    liste_noeud *copie = noeud->fils;
+    while (copie != NULL){
+        print_aux(copie->no);
+        
+        copie=copie->succ;
+    }
+}
+
+// affcihage qui va en profndeur le plu spossible : préfixe ?
+void print(){
+    print_aux(noeudCourant->racine);
+}
+    
